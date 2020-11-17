98541b6287aea818c0cd63c12204fca3fde95e7c
nituldeori <42697071+nituldeori@users.noreply.github.com>
2020-11-17 23:18:59 +0530

    Update EmployeeAttendance.java

--- src/employee/EmployeeAttendance.java
+++ src/employee/EmployeeAttendance.java
@@ -3,6 +3,9 @@ package employee;
 public class EmployeeAttendance {
 	public static void main(String[] args) {
 		System.out.println("********** Welcome to Employee Details ************");
+		int i=0;
+		System.out.println("Employee is present");
+
 	}
 
 }
