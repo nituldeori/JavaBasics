a0ef94bef8ebf92c5f473752502b7bb040c75e64
unknown <nituldeori63@gmail.com>
2020-11-17 19:56:36 +0530

    first commit

---
+++ src/employee/EmployeeAttendance.java
@@ -0,0 +1,8 @@
+package employee;
+
+public class EmployeeAttendance {
+	public static void main(String[] args) {
+		System.out.println("********** Welcome to Employee Details ************");
+	}
+
+}
